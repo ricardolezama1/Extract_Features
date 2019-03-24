93ad0524b3c66a460e236b7ff6e285f1c027bff6
Ricardo Lezama <lezama@lacartita.com>
2019-03-23 23:29:05 -0700

    Update data_extraction.py
    Adding a command line argument.

--- data_extraction.py
+++ data_extraction.py
@@ -4,12 +4,12 @@
 import pandas as pd  
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
-
+import sys
 #initialize TF IDF vector object
 vectorizer = TfidfVectorizer()
 
 #input file 
-language_data = open("test.txt", 'r')
+language_data = open(sys.argv[0], 'r')
 
 Vectora = vectorizer.fit_transform(language_data)
 a = vectorizer.get_feature_names()
@@ -17,3 +17,5 @@ a = vectorizer.get_feature_names()
 #list nltk stopwords
 stop = stopwords.words('english')
 document = [i for i in a if i not in stop]
+
+#evaluate 
